7059f89709c0a4f21a63854cf2192838576df4f8
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-05-27 20:07:02 +0200

    controller for auth and todo endpoints

---
+++ controller/todo.controller.js
@@ -0,0 +1,13 @@
+const ToDoServices = require('../services/todo.services');
+
+exports.createTodo = async (req, res, next) => {
+    try {
+        const {userId, title, description} = req.body;
+        let todo = await ToDoServices.createTodo(userId,title,description);
+
+        res.json({status: true, success: todo});
+
+    } catch(error) {
+        next(error);
+    }
+}
